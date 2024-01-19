19219bb61af4f6c7e11cf95040b4ea9cd5b22fae
StefaniaGuta <142820560+StefaniaGuta@users.noreply.github.com>
2024-01-19 07:02:55 -0800

    task 9

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,5 +1,14 @@
+const toggleButton = document.querySelector(".change-color");
+const body = document.body;
+const colorChanged = document.querySelector(".color");
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
-}
+  }
+  
+  toggleButton.addEventListener("click", e => {
+  body.style.backgroundColor = getRandomHexColor();
+  colorChanged.textContent = getRandomHexColor();
+  });
