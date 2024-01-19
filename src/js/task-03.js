fd8e0a2d12c3d2b9a9f532cd1f87bd6a0badc96c
StefaniaGuta <142820560+StefaniaGuta@users.noreply.github.com>
2024-01-19 06:57:00 -0800

    task 3

--- src/js/task-03.js
+++ src/js/task-03.js
@@ -12,3 +12,11 @@ const images = [
     alt: 'Group of Horses Running',
   },
 ];
+
+const galleryList = document.querySelector(".gallery");
+      const markup = images.map((image) =>`<li>
+      <img class="img" src="${image.url}" alt="${image.alt}">
+    </li>`);
+    
+    galleryList.insertAdjacentHTML('beforeend', markup);
+    console.log(markup);
