df107617d11fcf3ccf713131273b4896b1378010
StefaniaGuta <142820560+StefaniaGuta@users.noreply.github.com>
2024-01-19 07:01:45 -0800

    task 8

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,23 @@
+const loginForm = document.querySelector(".login-form");
 
+loginForm.addEventListener("submit", e => {
+    e.preventDefault();
+    console.log(e);
+
+    const inputs = document.querySelector(".login-form").elements;
+    const emailInput = inputs[0];
+    const passwordInput = inputs[1];
+  
+    
+   if(emailInput.value === '' || passwordInput.value === ''){
+    alert(`All fields must be filled in!`)
+} else {
+    const dataForm = {
+        email: emailInput.value,
+        password: passwordInput.value
+    }
+    console.log('User data:', dataForm);
+    alert("The form has been submitted successfully!");
+    loginForm.reset();
+   }
+});
